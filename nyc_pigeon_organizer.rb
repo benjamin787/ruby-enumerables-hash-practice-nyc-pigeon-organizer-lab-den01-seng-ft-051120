9ada57c2c6f47c37965a848a4648bc130b4f070f
Benjamin Rowe <browe495@gmail.com>
2020-04-27 22:44:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   pigeons = data.inject { | memo, (key, value) |
     
     value.inject { | memo, (color, names_array) |
-    binding.pry 
+    
       names_array.inject { |memo, x|
         
         memo = memo + x
