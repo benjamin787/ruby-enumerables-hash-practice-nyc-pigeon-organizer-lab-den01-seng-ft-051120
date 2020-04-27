03a73358ebe3372dbd7ea5dd6962d09be4a38916
Benjamin Rowe <browe495@gmail.com>
2020-04-27 22:40:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,45 @@
+require "pry" 
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  # make array of names 
+  
+  pigeons = data.inject { | memo, (key, value) |
+    
+    value.inject { | memo, (color, names_array) |
+      names_array.inject { |memo, x|
+        
+        memo = memo + x
+        memo
+      }
+      memo
+      
+    }
+    
+    memo
+  }
+  binding.pry
+    
+# remove repeats
+
+  pigeons = pigeons.uniq
+  
+  # form template hash 
+  
+  answer = pigeons.reduce { | omem, a |
+  
+    omem[a] = { :color => [], :gender => [], :lives => [] }
+    omem
+  }
+  
+# fill hash
+
+  data.each { |(k, v)|
+    v.each { |(color, names_array)|
+      names_array.each { |b|
+        answer[b][k] << [color.to_s] 
+      }
+    }
+  }
+  
+  answer
 end
